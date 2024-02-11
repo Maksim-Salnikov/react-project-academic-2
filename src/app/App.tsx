44eda90506e6f79cb671e1153b627a2ed36417ce
Maksim-Salnikov <salomaksim123@gmail.com>
2024-02-11 21:50:10 +0300

    leasson 1.10 (10): Внедрил методологию feature-sliced.design

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,23 @@
 import { Route, Routes } from "react-router-dom";
-import { AboutPageAsync } from "../pages/AboutPage/AboutPage.async";
-import { MainPageAsync } from "../pages/MainPage/MainPage.async";
 import { Link } from "react-router-dom";
-import { Suspense, useContext, useState } from "react";
-import { Theme, ThemeContext } from "../theme/ThemeContext";
-import { useTheme } from "../theme/useTheme";
+import { Suspense } from "react";
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from "app/providers/ThemeProvider/lib/useTheme";
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames("app", {}, [theme])}>
       <button onClick={toggleTheme}>THEME</button>
       <Link to={"/"}>Главная Страница</Link>
       <Link to={"/about"}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path={"/about"} element={<AboutPageAsync />} />
-          <Route path={"/"} element={<MainPageAsync />} />
+          <Route path={"/about"} element={<AboutPage />} />
+          <Route path={"/"} element={<MainPage />} />
         </Routes>
       </Suspense>
     </div>
