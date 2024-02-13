942a19977391a208e70de1d481bd7c6feeee3a11
Maksim-Salnikov <salomaksim123@gmail.com>
2024-02-13 21:16:22 +0300

    leasson 2.1(11): Добавил Route config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,7 @@
-import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { Suspense } from "react";
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/ThemeProvider/lib/useTheme";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
+import { AppRouter } from "./providers/router";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -14,12 +11,7 @@ const App = () => {
       <button onClick={toggleTheme}>THEME</button>
       <Link to={"/"}>Главная Страница</Link>
       <Link to={"/about"}>О сайте</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={"/about"} element={<AboutPage />} />
-          <Route path={"/"} element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
