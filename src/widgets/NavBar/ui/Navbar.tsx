310d7e69125ab5ff791d20b87be611b80e98cc88
Maksim-Salnikov <salomaksim123@gmail.com>
2024-03-13 17:43:14 +0300

    fix Navbar and Screenshots

--- src/widgets/NavBar/ui/Navbar.tsx
+++ src/widgets/NavBar/ui/Navbar.tsx
@@ -1,15 +1,11 @@
 import { classNames } from 'shared/lib/classNames/classNames'
 import cls from './Navbar.module.scss'
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink'
-import { useTranslation } from 'react-i18next'
 
 interface NavBarProps {
   className?: string
 }
 
 export const NavBar = ({ className }: NavBarProps) => {
-  const { t } = useTranslation()
-
   return (
     <div className={classNames(cls.navbar, {}, [className])}>
       <div className={cls.links}>/</div>
