929ba495806ba25e2b48f1d5109f5ecdcd80f5aa
Maksim-Salnikov <salomaksim123@gmail.com>
2024-03-21 10:14:44 +0300

    leasson 4.3(31) Исправление темы в модальном окне

--- .eslintrc.js
+++ .eslintrc.js
@@ -55,6 +55,7 @@ module.exports = {
     'max-len': ['error', { ignoreComments: true, code: 100 }],
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'error',
+    'react-hooks/exhaustive-deps': 'off',
   },
   overrides: [
     {
